40edef6d85e100c5e44c956ac29691ea75189678
Ekaterina <87katrod@gmail.com>
2018-12-12 00:01:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+function getFirstSelector(selector) {
+  return   document.querySelector(selector);
+}
+
+function nestedTarget() {
+ return   document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  let result =  document.querySelectorAll('.ranked-list');
+ for (let i=0; i<result.length; i++){
+   result[i].innerHTML = parseInt(result[i].innerHTML)+n;
+ }
+
+
+}
