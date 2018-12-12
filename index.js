f972ae626b30a8ab43f1a709c3c459147c2e5196
Ekaterina <87katrod@gmail.com>
2018-12-12 00:58:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector) {
+  return   document.querySelector(selector);
+}
+
+function nestedTarget() {
+ return   document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  let result =  document.querySelectorAll('.ranked-list');
+ for (let i=0; i<result.length; i++){
+   result[i].innerHTML = parseInt(result[i].innerHTML)+n;
+ }
+}
+
+
+const deepestChild = () => {
+  var lis=document.querySelectorAll("#grand-node div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
+};
